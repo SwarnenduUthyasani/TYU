a3ded10a39d48fc86619730e5f65cbbdd489ae39
2355301 <2355301@LTIN486332>
2024-11-13 15:22:39 +0530

    Add project files.

---
+++ Com.Cognizant.Truyum.Model/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Com.Cognizant.Truyum.Model
+{
+    public class MenuItem
+    {
+        private long id { get; set; }
+        private string name { get; set; }
+        private float price { get; set; }
+        private Boolean active { get; set; }
+        private DateOnly dateOfLaunch { get; set; }
+        private string category { get; set; }
+        private Boolean freeDelivery { get; set; }
+
+        public MenuItem()
+        {
+            id = this.id;
+            name = this.name;
+            price = this.price;
+            active = this.active;
+            dateOfLaunch = this.dateOfLaunch;
+            category = this.category;
+            freeDelivery = this.freeDelivery;
+        }
+
+    }
+}
---
+++ TruyumOnline/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TruyumOnline
+{
+    public class Class1
+    {
+
+    }
+}
